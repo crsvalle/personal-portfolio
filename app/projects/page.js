fb7b10213127ca8305158ba752a390f2ebecd378
crsvalle <cristian.valle@pursuit.org>
2025-01-17 14:25:30 -0500

    project page work in progress

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,12 @@
 
 export default function Projects() {
   return (
-    <div>Work in Progess</div>
+    <main className="min-h-screen px-6 py-10  text-white">
+      <div className="flex flex-col items-center gap-y-20">
+        <section className="flex flex-col-reverse items-start gap-x-10 mt-36 md:flex-row md:items-center max-w-2xl mx-auto" id="about">
+          work in progress
+        </section>
+      </div>
+    </main>
   )
 }
