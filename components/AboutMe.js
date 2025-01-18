95ae4293e0e92dfa7a50748345a74139b727cff9
crsvalle <cristian.valle@pursuit.org>
2025-01-18 00:29:11 -0500

    fixed text error

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -3,21 +3,21 @@ import Image from 'next/image'
 export default function AboutMe() {
   return (
     <section className="flex flex-col-reverse items-start gap-x-10 mt-36 md:flex-row md:items-center max-w-2xl mx-auto" id="about">
-      <section className="flex flex-col md:flex-row items-center gap-6 md:gap-10" >
-        <div className="flex-1 text-left ">
+      <section className="flex flex-col md:flex-row items-center gap-6 md:gap-10">
+        <div className="flex-1 text-left">
           <h1 className="text-3xl font-bold decoration-border/75 decoration-2 underline-offset-8">
-            Hey, I&#39;m Cristian Valle.
+            Hey, I&apos;m Cristian Valle.
           </h1>
           <p className="mt-3 font-light text-muted-foreground">
-            I&#39;m a software engineer based in New York City. I&#39;m
-            passionate about learning new technologies. Currently, I'm 
+            I&apos;m a software engineer based in New York City. I&apos;m
+            passionate about learning new technologies. Currently, I&apos;m 
             working on a project that leverages image recognition to identify 
             car models with impressive accuracy.
           </p>
         </div>
         <div className="relative flex-shrink-0">
           <Image
-            className="rounded-lg  hover:grayscale-0 transition duration-300"
+            className="rounded-lg hover:grayscale-0 transition duration-300"
             src="/cv.jpg"
             alt="Cristian V"
             width={175}
@@ -26,8 +26,6 @@ export default function AboutMe() {
           />
         </div>
       </section>
-
     </section>
   );
 }
-
